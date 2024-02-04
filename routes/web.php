7d5c977f39536740c81a1dd2ed79bb78b77fd666
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2024-02-04 19:36:53 -0300

    FEAT: Adicionadas rotas de login e criação de usuários

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\{
+    AuthenticatedSessionController,
+    RegisteredUserController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +21,41 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+                ->name('login.create');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+        ->name('login');
+
+    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    //             ->name('password.request');
+
+    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    //             ->name('password.email');
+
+    // Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+    //             ->name('password.reset');
+
+    // Route::post('reset-password', [NewPasswordController::class, 'store'])
+    //             ->name('password.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })
+    ->name('dashboard');
 });
